e972c842ab8876f403ce9b898d31ec7df7b6a10e
skdat6 <dobretheo@gmail.com>
2023-03-31 12:31:09 +0300

    fixed some expect issues, refactor

--- tests/UItest.spec.js
+++ tests/UItest.spec.js
@@ -3,7 +3,7 @@ const { test, expect } = require('@playwright/test');
 //tests in a single file run sequencially
 //tests in separate fiels run in parallel
 
-test('Login Page', async ({ browser }) => {
+test('Login Page', async({ browser }) => {
     // Test code goes here, add async to function - sageata e anonymous function, fara nume
     //open browser
     //await
@@ -22,32 +22,43 @@ test('Login Page', async ({ browser }) => {
 
 });
 
-test('Sauce Demo Invalid Login', async ({ page }) => {
+test('Sauce Demo Invalid Login', async({ page }) => {
 
     await page.goto('https://www.saucedemo.com/');
     await page.locator('//*[@id="user-name"]').type('standard_user222');
     await page.locator('//*[@id="password"]').type('secret_sauce');
     await page.locator('//*[@id="login-button"]').click();
-    console.log(await page.locator('//*[@id="login_button_container"]/div/form/div[3]/h3').textContent());
-
-
-
+    const message = await page.locator('//*[@id="login_button_container"]/div/form/div[3]/h3').textContent();
+    console.log(message);
+    expect(await message == 'Epic sadface: Username and password do not match any user in this service')
 
 });
 
-test('Sauce Demo Valid Login', async ({ page }) => {
+test('Sauce Demo Locked out User', async({ page }) => {
+
+    await page.goto('https://www.saucedemo.com/');
+    await page.locator('//*[@id="user-name"]').type('locked_out_user');
+    await page.locator('//*[@id="password"]').type('secret_sauce');
+    await page.locator('//*[@id="login-button"]').click();
+    const message = await page.locator('//*[@id="login_button_container"]/div/form/div[3]/h3').textContent();
+    console.log(message);
+    expect(message).toEqual('Epic sadface: Sorry, this user has been locked out.');
+
+});
+
+test('Sauce Demo Valid Login', async({ page }) => {
 
     await page.goto('https://www.saucedemo.com/');
     await page.locator('//*[@id="user-name"]').type('standard_user');
     await page.locator('//*[@id="password"]').type('secret_sauce');
     await page.locator('//*[@id="login-button"]').click();
-    expect(await page.locator('//*[@id="header_container"]/div[2]/span').textContent() == 'Products');
+    expect(await page.locator('//*[@id="header_container"]/div[2]/span').textContent()).toEqual('Products');
 
 
 
 });
 
-test('Add first item', async ({ page }) => {
+test('Add first item', async({ page }) => {
 
     await page.goto('https://www.saucedemo.com/');
     await page.locator('//*[@id="user-name"]').type('standard_user');
@@ -59,11 +70,11 @@ test('Add first item', async ({ page }) => {
     const nthBtn = page.locator(nthButtonSelector);
     await nthBtn.click();
     console.log(await nthBtn.textContent());
-    expect(await nthBtn.textContent() == 'Remove');
+    expect(await nthBtn.textContent()).toEqual('Remove');
 
 });
 
-test('Add second item', async ({ page }) => {
+test('Add second item', async({ page }) => {
 
     await page.goto('https://www.saucedemo.com/');
     await page.locator('//*[@id="user-name"]').type('standard_user');
@@ -75,11 +86,11 @@ test('Add second item', async ({ page }) => {
     const nthBtn = page.locator(nthButtonSelector);
     await nthBtn.click();
     console.log(await nthBtn.textContent());
-    expect(await nthBtn.textContent() == 'Remove');
+    expect(await nthBtn.textContent()).toEqual('Remove');
 
 });
 
-test('Select filter option', async ({ page }) => {
+test('Select filter option', async({ page }) => {
 
     await page.goto('https://www.saucedemo.com/');
     await page.locator('//*[@id="user-name"]').type('standard_user');
@@ -88,10 +99,9 @@ test('Select filter option', async ({ page }) => {
     const dropdown = page.locator('select.product_sort_container');
     await dropdown.selectOption('za');
 
-
 });
 
-test('Items added match cart badge', async ({ page }) => {
+test('Items added match cart badge', async({ page }) => {
 
     await page.goto('https://www.saucedemo.com/');
     await page.locator('//*[@id="user-name"]').type('standard_user');
